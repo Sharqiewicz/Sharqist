de09f9c3420810fb46c3e40bcdea7b29b2b5d600
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-08-07 08:24:12 +0100

    add basic navbar

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { invoke } from '@tauri-apps/api/tauri'
 
 import './index.css'
 import { Sidebar } from './components/Sidebar/Sidebar'
+import { Navbar } from './components/Navbar/Navbar'
 
 function App() {
   const [greetMsg, setGreetMsg] = useState('')
@@ -14,33 +15,36 @@ function App() {
   }
 
   return (
-    <div className='container columns-2 flex flex-row'>
-      <div className='basis-1/3'>
-        <Sidebar />
-      </div>
+    <div className=''>
+      <Navbar />
+      <div className='container columns-2 flex flex-row'>
+        <div className='basis-1/3'>
+          <Sidebar />
+        </div>
 
-      <div className='basis-2/3'>
-        <h1>Bonjour</h1>
-        <div className='row'></div>
+        <div className='basis-2/3'>
+          <h1>Bonjour</h1>
+          <div className='row'></div>
 
-        <p>Aucun taches pour aujourd'hui</p>
+          <p>Aucun taches pour aujourd'hui</p>
 
-        <form
-          className='row'
-          onSubmit={e => {
-            e.preventDefault()
-            greet()
-          }}
-        >
-          <input
-            id='greet-input'
-            onChange={e => setName(e.currentTarget.value)}
-            placeholder='Enter a name...'
-          />
-          <button type='submit'>Greet</button>
-        </form>
+          <form
+            className='row'
+            onSubmit={e => {
+              e.preventDefault()
+              greet()
+            }}
+          >
+            <input
+              id='greet-input'
+              onChange={e => setName(e.currentTarget.value)}
+              placeholder='Enter a name...'
+            />
+            <button type='submit'>Greet</button>
+          </form>
 
-        <p>{greetMsg}</p>
+          <p>{greetMsg}</p>
+        </div>
       </div>
     </div>
   )
