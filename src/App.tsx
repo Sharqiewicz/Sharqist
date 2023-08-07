9fc35accea57fbf95ad80cdb9234ef09b15b6ef3
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-08-07 08:24:12 +0100

    add basic sidebar

--- src/App.tsx
+++ src/App.tsx
@@ -1,53 +1,49 @@
-import { useState } from "react";
-import { invoke } from "@tauri-apps/api/tauri";
+import { useState } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
 
-import "./index.css";
+import './index.css'
+import { Sidebar } from './components/Sidebar/Sidebar'
 
 function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+  const [greetMsg, setGreetMsg] = useState('')
+  const [name, setName] = useState('')
 
   async function greet() {
     // Learn more about Tauri commands at https://tauri.app/v1/guides/features/command
-    setGreetMsg(await invoke("greet", { name }));
+    setGreetMsg(await invoke('greet', { name }))
   }
 
   return (
-    <div className="container columns-2 flex flex-row">
-
-
-      <div className="basis-1/3">
-        <h2>Aujourd'hui</h2>
+    <div className='container columns-2 flex flex-row'>
+      <div className='basis-1/3'>
+        <Sidebar />
       </div>
 
-      <div className="basis-2/3">
-      <h1>Bonjour</h1>
-      <div className="row">
+      <div className='basis-2/3'>
+        <h1>Bonjour</h1>
+        <div className='row'></div>
 
-      </div>
+        <p>Aucun taches pour aujourd'hui</p>
 
-      <p>Aucun taches pour aujourd'hui</p>
+        <form
+          className='row'
+          onSubmit={e => {
+            e.preventDefault()
+            greet()
+          }}
+        >
+          <input
+            id='greet-input'
+            onChange={e => setName(e.currentTarget.value)}
+            placeholder='Enter a name...'
+          />
+          <button type='submit'>Greet</button>
+        </form>
 
-      <form
-        className="row"
-        onSubmit={(e) => {
-          e.preventDefault();
-          greet();
-        }}
-      >
-        <input
-          id="greet-input"
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
-        />
-        <button type="submit">Greet</button>
-      </form>
-
-
-      <p>{greetMsg}</p>
+        <p>{greetMsg}</p>
       </div>
     </div>
-  );
+  )
 }
 
-export default App;
+export default App
