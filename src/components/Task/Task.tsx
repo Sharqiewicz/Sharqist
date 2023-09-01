fb9bbb8eb9de53a95ea60fec1572a5b5d80c985e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-09-01 08:24:22 +0100

    show cursor pointer for dropdown items

--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -30,7 +30,7 @@ export const Task = (task: ITask) => {
     <div className='relative w-full p-6 mb-3 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700'>
       <Dropdown
         options={dropdownOptions}
-        additionalStyles='absolute right-4 top-4 flex justify-end flex-col items-end'
+        additionalStyles='absolute right-4 top-4 flex justify-end flex-col items-end cursor-pointer'
       />
       <h1 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>
         {name}
