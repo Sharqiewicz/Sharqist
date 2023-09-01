e4367670287be63b0034e569cea8406ab8c29a89
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-09-01 08:24:22 +0100

    add dropdown with basic design

---
+++ src/components/Dropdown/Dropdown.tsx
@@ -0,0 +1,63 @@
+import { FC, useState } from 'react'
+
+export interface DropdownOption {
+  name: string
+  icon: string
+  onClick: () => void
+}
+interface DropdownProps {
+  options: DropdownOption[]
+}
+
+const Dropdown: FC<DropdownProps> = ({ options }) => {
+  const [isOpen, setIsOpen] = useState(false)
+
+  const toggleIsOpen = () => {
+    setIsOpen(state => !state)
+  }
+
+  return (
+    <div>
+      <button
+        onClick={toggleIsOpen}
+        id='dropdownMenuIconHorizontalButton'
+        data-dropdown-toggle='dropdownDotsHorizontal'
+        className='inline-flex items-center p-2 text-sm font-medium text-center text-gray-900 bg-white rounded-lg hover:bg-gray-100 focus:ring-4 focus:outline-none dark:text-white focus:ring-gray-50 dark:bg-gray-800 dark:hover:bg-gray-700 dark:focus:ring-gray-600'
+        type='button'
+      >
+        <svg
+          className='w-5 h-5'
+          aria-hidden='true'
+          xmlns='http://www.w3.org/2000/svg'
+          fill='currentColor'
+          viewBox='0 0 16 3'
+        >
+          <path d='M2 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Zm6.041 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3ZM14 0a1.5 1.5 0 1 1 0 3 1.5 1.5 0 0 1 0-3Z' />
+        </svg>
+      </button>
+
+      {isOpen ? renderDropdownOptions(options) : <></>}
+    </div>
+  )
+}
+
+const renderDropdownOptions = (options: DropdownOption[]) => (
+  <div
+    id='dropdownDotsHorizontal'
+    className='z-10 divide-y bg-white divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700 dark:divide-gray-600'
+  >
+    <ul
+      className='py-2 text-sm text-gray-700 dark:text-gray-200'
+      aria-labelledby='dropdownMenuIconHorizontalButton'
+    >
+      {options.map(option => (
+        <li>
+          <p className='block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white'>
+            {option.name}
+          </p>
+        </li>
+      ))}
+    </ul>
+  </div>
+)
+export { Dropdown }
--- src/components/Task/Task.tsx
+++ src/components/Task/Task.tsx
@@ -1,5 +1,6 @@
 import moment from 'moment'
 import { ITask } from '../../interfaces/ITask'
+import { Dropdown, DropdownOption } from '../Dropdown/Dropdown'
 
 export const Task = (task: ITask) => {
   const { name, description, date } = task
@@ -9,8 +10,25 @@ export const Task = (task: ITask) => {
     isBeforeToday ? 'text-red-600' : 'text-gray-500 dark:text-gray-400'
   }`
 
+  const editTask = () => {}
+  const deleteTask = () => {}
+
+  const dropdownOptions: DropdownOption[] = [
+    {
+      name: 'Edit',
+      icon: 'edit',
+      onClick: editTask,
+    },
+    {
+      name: 'Delete',
+      icon: 'delete',
+      onClick: deleteTask,
+    },
+  ]
+
   return (
     <div className='p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 w-full mb-3'>
+      <Dropdown options={dropdownOptions} />
       <h1 className='mb-2 text-2xl font-semibold tracking-tight text-gray-900 dark:text-white'>
         {name}
       </h1>
