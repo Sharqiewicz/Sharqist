522c890d77350512236e3d80738af2daff09c1f0
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-09-20 08:24:29 +0100

    change AddTaskModal width

--- src/modals/AddTaskModal/AddTaskModal.tsx
+++ src/modals/AddTaskModal/AddTaskModal.tsx
@@ -102,7 +102,7 @@ const renderTaskModal = ({
   >
     <div className='fixed top-0 bottom-0 left-0 right-0 bg-black opacity-80'></div>
     <div className='relative w-full max-w-md max-h-full p-4'>
-      <div className='fixed bg-gray-900 rounded-lg shadow z-60 top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4'>
+      <div className='fixed w-2/5 bg-gray-900 rounded-lg shadow z-60 top-1/2 left-1/2 -translate-y-2/4 -translate-x-2/4'>
         {renderHeader({ closeModal })}
         <form className='p-4 md:p-5' onSubmit={onSubmit}>
           {renderTaskName(handleChange)}
