7ccfb1135d3092b3ef735382438f96e4575de632
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-09-28 08:24:33 +0100

    add TodayPage

---
+++ src/pages/TodayPage/TodayPage.tsx
@@ -0,0 +1,11 @@
+import { MainTemplate } from '../../templates/MainTemplate'
+import { TodayView } from '../../views/TodayView/TodayView'
+
+export default function TodayPage() {
+  return (
+    <MainTemplate>
+      <h1>HAHAHA</h1>
+      <TodayView />
+    </MainTemplate>
+  )
+}
