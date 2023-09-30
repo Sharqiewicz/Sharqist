90b79c61e44c003e5f44df1765fcf69aed905794
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-09-30 08:24:33 +0100

    remove unnecessary header

--- src/pages/TodayPage/TodayPage.tsx
+++ src/pages/TodayPage/TodayPage.tsx
@@ -4,7 +4,6 @@ import { TodayView } from '../../views/TodayView/TodayView'
 export default function TodayPage() {
   return (
     <MainTemplate>
-      <h1>HAHAHA</h1>
       <TodayView />
     </MainTemplate>
   )
