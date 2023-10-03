484e83b18dc678d712672919a19c8906b9ee5ced
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-10-03 08:24:34 +0100

    add useFetchTasks custom hook

---
+++ src/hooks/useFetchTasks.tsx
@@ -0,0 +1,27 @@
+import { useState, useEffect } from 'react'
+import { invoke } from '@tauri-apps/api/tauri'
+import moment from 'moment'
+
+import { ITask } from '../interfaces/ITask'
+
+const useFetchTasks = (taskType: string) => {
+  const [tasks, setTasks] = useState<ITask[]>([])
+
+  const fetchTasks = async () => {
+    const allTasks: ITask[] = await invoke(taskType)
+
+    allTasks.sort((a: ITask, b: ITask) => {
+      return moment(a.date).isAfter(b.date) ? 1 : -1
+    })
+
+    setTasks(allTasks)
+  }
+
+  useEffect(() => {
+    fetchTasks()
+  }, [])
+
+  return tasks
+}
+
+export default useFetchTasks
