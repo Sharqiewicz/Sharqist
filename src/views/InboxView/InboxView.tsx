309a57162fb4f9977fcd35b6b16ebfc5c617945b
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-10-04 08:24:34 +0100

    make taskList code more reusable

--- src/views/InboxView/InboxView.tsx
+++ src/views/InboxView/InboxView.tsx
@@ -1,41 +1,19 @@
-import { useEffect, useState } from 'react'
-import { invoke } from '@tauri-apps/api/tauri'
 import { ITask } from '../../interfaces/ITask'
-import { Task } from '../../components/Task/Task'
 import { useModal } from '../../modals/ModalsContext'
-import moment from 'moment'
+import { TasksList } from '../../components/TaskList/TaskList'
+import useFetchTasks from '../../hooks/useFetchTasks'
 
 function InboxView() {
   const { MODALS, openModal } = useModal()
 
-  const [tasks, setTasks] = useState<ITask[]>([])
-
-  useEffect(() => {
-    ;(async () => {
-      const allTasks: ITask[] = await invoke('get_all_tasks')
-
-      allTasks.sort((a: ITask, b: ITask) => {
-        return moment(a.date).isAfter(b.date) ? 1 : -1
-      })
-
-      setTasks(allTasks)
-    })()
-  }, [])
-
-  const renderTasks = () => {
-    if (tasks.length) {
-      return tasks.map(task => <Task {...task} />)
-    } else {
-      return <p>Aucun taches pour aujourd'hui</p>
-    }
-  }
+  const tasks: ITask[] = useFetchTasks('get_all_tasks')
 
   return (
     <>
       <h1 className='mb-3 text-2xl font-bold text-gray-700'>Inbox</h1>
       <div className='row'></div>
 
-      {renderTasks()}
+      <TasksList tasks={tasks} />
 
       <hr className='my-5' />
 
