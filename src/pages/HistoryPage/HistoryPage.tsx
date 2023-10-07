e4d8dcd75837ab481e24a72684d90a8e94d1128b
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-10-07 08:24:35 +0100

    add HistoryPage

---
+++ src/pages/HistoryPage/HistoryPage.tsx
@@ -0,0 +1,12 @@
+import { MainTemplate } from '../../templates/MainTemplate'
+import { HistoryView } from '../../views'
+
+function HistoryPage() {
+  return (
+    <MainTemplate>
+      <HistoryView />
+    </MainTemplate>
+  )
+}
+
+export { HistoryPage }
