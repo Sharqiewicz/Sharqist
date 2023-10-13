995cad6a4f325a088f2e8d3decb3544e03432511
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-10-13 08:24:37 +0100

    add UpcomingPage

---
+++ src/pages/UpcomingPage/UpcomingPage.tsx
@@ -0,0 +1,12 @@
+import { MainTemplate } from '../../templates/MainTemplate'
+import { UpcomingView } from '../../views'
+
+function UpcomingPage() {
+  return (
+    <MainTemplate>
+      <UpcomingView />
+    </MainTemplate>
+  )
+}
+
+export { UpcomingPage }
