f5e3160ef9b003ade8d2ad4def778b1af1cf745e
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-10-13 08:24:37 +0100

    add UpcomingView

---
+++ src/views/UpcomingView/UpcomingView.tsx
@@ -0,0 +1,22 @@
+import { ITask } from '../../interfaces/ITask'
+import { TasksList } from '../../components/TaskList/TaskList'
+import { useFetchTasks } from '../../hooks/useFetchTasks'
+
+function UpcomingView() {
+  const {
+    tasks,
+    forceTasksFetchUpdate,
+  }: { tasks: ITask[]; forceTasksFetchUpdate: () => void } =
+    useFetchTasks('get_future_tasks')
+
+  return (
+    <>
+      <h1 className='mb-3 text-2xl font-bold text-gray-700'>Today</h1>
+      <div className='row'></div>
+
+      <TasksList tasks={tasks} forceTasksFetchUpdate={forceTasksFetchUpdate} />
+    </>
+  )
+}
+
+export { UpcomingView }
