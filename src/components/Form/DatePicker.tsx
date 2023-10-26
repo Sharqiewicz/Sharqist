05bd0f6f73b0d4d0d45fea8ab8475643597dbaa8
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-10-26 08:24:41 +0100

    extract DatePicker to separate component

---
+++ src/components/Form/DatePicker.tsx
@@ -0,0 +1,29 @@
+import { ReactDatePicker } from 'react-datepicker'
+import 'react-datepicker/dist/react-datepicker.css'
+
+export const DatePicker = ({
+  date,
+  setDate,
+}: {
+  date: Date
+  setDate: (date: Date) => void
+}) => (
+  <div className='mt-5 w-44'>
+    <label
+      htmlFor='date'
+      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+    >
+      Date
+    </label>
+    <ReactDatePicker
+      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+      selected={date}
+      onChange={date => {
+        setDate(date as Date)
+      }}
+      dateFormat={'dd.MM.yyyy'}
+      required={true}
+      id='date'
+    />
+  </div>
+)
