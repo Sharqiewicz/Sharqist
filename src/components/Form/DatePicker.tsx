cfc675ff6579c9ac294ca1ba5231e8baf6ccc999
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-11-02 08:24:43 +0100

    fix DatePicker import

--- src/components/Form/DatePicker.tsx
+++ src/components/Form/DatePicker.tsx
@@ -1,7 +1,7 @@
-import { ReactDatePicker } from 'react-datepicker'
+import DatePicker from 'react-datepicker'
 import 'react-datepicker/dist/react-datepicker.css'
 
-export const DatePicker = ({
+export const CustomDatePicker = ({
   date,
   setDate,
 }: {
@@ -15,7 +15,7 @@ export const DatePicker = ({
     >
       Date
     </label>
-    <ReactDatePicker
+    <DatePicker
       className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
       selected={date}
       onChange={date => {
@@ -27,3 +27,5 @@ export const DatePicker = ({
     />
   </div>
 )
+
+export { CustomDatePicker as DatePicker }
