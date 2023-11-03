07b6a23c46238b5ade54656c1f2c46834991c9f6
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-11-03 08:24:44 +0100

    add TextInput component

---
+++ src/components/Form/TextInput.tsx
@@ -0,0 +1,25 @@
+import { ChangeEvent } from 'react'
+
+export const TextInput = ({
+  handleChange,
+}: {
+  handleChange: (event: ChangeEvent) => void
+}) => (
+  <div className='col-span-2'>
+    <label
+      htmlFor='name'
+      className='block mb-2 text-sm font-medium text-gray-900 dark:text-white'
+    >
+      Task name
+    </label>
+    <input
+      type='text'
+      name='name'
+      id='name'
+      className='bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500'
+      placeholder='Type product name'
+      required={true}
+      onChange={handleChange}
+    />
+  </div>
+)
