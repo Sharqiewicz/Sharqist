0fe3c288553bdccace2107c63b80c3eb9b38f26b
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-11-18 08:24:50 +0100

    create new struct(s) for projects

--- src-tauri/src/structs/mod.rs
+++ src-tauri/src/structs/mod.rs
@@ -14,3 +14,18 @@ pub struct Task {
     pub date: String,
     pub is_done: bool,
 }
+
+#[derive(serde::Serialize)]
+pub struct NewProject {
+    pub name: String,
+    pub description: String,
+    pub color: String,
+}
+
+#[derive(serde::Serialize)]
+pub struct Project {
+    pub id: i32,
+    pub name: String,
+    pub description: String,
+    pub color: String,
+}
