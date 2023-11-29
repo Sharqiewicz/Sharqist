31691e699949877f11e51c410252f713a153820d
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-11-29 08:24:53 +0100

    add ProjectsPage

---
+++ src/pages/ProjectsPage/ProjectsPage.tsx
@@ -0,0 +1,12 @@
+import { MainTemplate } from '../../templates/MainTemplate'
+import { ProjectsView } from '../../views'
+
+function ProjectsPage() {
+  return (
+    <MainTemplate>
+      <ProjectsView />
+    </MainTemplate>
+  )
+}
+
+export { ProjectsPage }
