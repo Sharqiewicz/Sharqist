dda7acff88dae2df0070e6766ce0a034b0f63b76
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-12-14 08:07:28 +0100

    remove 'product' from texts

--- src/components/Form/Description.tsx
+++ src/components/Form/Description.tsx
@@ -21,7 +21,7 @@ export const Description = ({
       id='description'
       rows={4}
       className='block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500'
-      placeholder='Write product description here'
+      placeholder='Write description here'
     ></textarea>
   </div>
 )
