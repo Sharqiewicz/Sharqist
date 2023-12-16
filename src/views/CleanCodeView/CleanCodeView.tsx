f8014148cb039183a10c6646a2760cd62784ed52
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-12-16 08:07:29 +0100

    Add CleanCodeView

---
+++ src/views/CleanCodeView/CleanCodeView.tsx
@@ -0,0 +1,10 @@
+function CleanCodeView() {
+  return (
+    <>
+      <h1 className='mb-3 text-2xl font-bold text-gray-700'>Clean Code</h1>
+      <div className='row'></div>
+    </>
+  )
+}
+
+export { CleanCodeView }
