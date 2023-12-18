9ef3bf483891c1b7d0a94fd61bee6e3b1bb527fc
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-12-18 08:07:30 +0100

    add reset button CleanCodeView

--- src/components/Button/ButtonPrimary.tsx
+++ src/components/Button/ButtonPrimary.tsx
@@ -1,9 +1,18 @@
 import { ReactNode } from 'react'
 
-const ButtonPrimary = ({ text, svg }: { text: string; svg: ReactNode }) => (
+const ButtonPrimary = ({
+  text,
+  svg,
+  onClick,
+}: {
+  text: string
+  svg: ReactNode
+  onClick?: () => void
+}) => (
   <button
     type='submit'
     className='flex items-center justify-center w-full mt-5 button-first'
+    onClick={onClick}
   >
     {svg}
     {text}
--- src/views/CleanCodeView/CleanCodeView.tsx
+++ src/views/CleanCodeView/CleanCodeView.tsx
@@ -1,3 +1,6 @@
+import { useState } from 'react'
+import { ButtonPrimary } from '../../components/Button/ButtonPrimary'
+
 function CleanCodeView() {
   const categories = [
     {
@@ -60,24 +63,52 @@ function CleanCodeView() {
     },
   ]
 
+  const [checkedPoints, setCheckedPoints] = useState<Record<string, boolean>>(
+    {},
+  )
+
+  const handleCheckboxChange = (categoryIndex: number, pointIndex: number) => {
+    setCheckedPoints({
+      ...checkedPoints,
+      [`${categoryIndex}-${pointIndex}`]:
+        !checkedPoints[`${categoryIndex}-${pointIndex}`],
+    })
+  }
+
+  const resetCheckboxes = () => {
+    setCheckedPoints({})
+  }
+
   return (
     <>
       <h1 className='mb-3 text-2xl font-bold text-gray-700'>Clean Code</h1>
       <div className='row'>
-        {categories.map((category, index) => (
+        {categories.map((category, categoryIndex) => (
           <div
-            key={index}
+            key={categoryIndex}
             className='flex flex-col items-start max-w-md p-6 mx-auto my-2 space-y-4 bg-white shadow-md rounded-xl'
           >
             <h2 className='text-xl font-semibold'>{category.title}</h2>
-            {category.points.map((point, index) => (
-              <div key={index}>
-                <h3 className='text-lg font-semibold'>{point.title}</h3>
-                <p className='mt-2 text-gray-600'>{point.description}</p>
+            {category.points.map((point, pointIndex) => (
+              <div key={pointIndex} className='flex items-start'>
+                <input
+                  type='checkbox'
+                  checked={
+                    checkedPoints[`${categoryIndex}-${pointIndex}`] || false
+                  }
+                  onChange={() =>
+                    handleCheckboxChange(categoryIndex, pointIndex)
+                  }
+                />
+                <div className='ml-2'>
+                  <h3 className='text-lg font-semibold'>{point.title}</h3>
+                  <p className='mt-2 text-gray-600'>{point.description}</p>
+                </div>
               </div>
             ))}
           </div>
         ))}
+        <ButtonPrimary svg='' text='Reset' onClick={resetCheckboxes} />
       </div>
     </>
   )
