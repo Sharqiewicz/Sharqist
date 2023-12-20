6112521d12964e6ed9a8187e2cc23eb4b784e8e3
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-12-21 00:10:59 +0100

    Add DesignPatternsPage

---
+++ src/pages/DesignPatternsPage/DesignPatternsPage.tsx
@@ -0,0 +1,12 @@
+import { MainTemplate } from '../../templates/MainTemplate'
+import { DesignPatternsView } from '../../views'
+
+function DesignPatternsPage() {
+  return (
+    <MainTemplate>
+      <DesignPatternsView />
+    </MainTemplate>
+  )
+}
+
+export { DesignPatternsPage }
