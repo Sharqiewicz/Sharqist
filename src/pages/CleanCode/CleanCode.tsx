1c8565b73017f3a60e8e88c2882906f83cfdb483
Kacper Szarkiewicz <szarkiewiczmail@gmail.com>
2023-12-20 08:25:01 +0100

    Add CleanCode Page

---
+++ src/pages/CleanCode/CleanCode.tsx
@@ -0,0 +1,12 @@
+import { MainTemplate } from '../../templates/MainTemplate'
+import { CleanCodeView } from '../../views'
+
+function CleanCodePage() {
+  return (
+    <MainTemplate>
+      <CleanCodeView />
+    </MainTemplate>
+  )
+}
+
+export { CleanCodePage }
